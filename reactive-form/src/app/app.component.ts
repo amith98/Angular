fdf28b6e765e7e3823f7b2ccd2e0e7fa9abb0fca
Amith <amithar1998@gmail.com>
2025-11-17 17:47:39 +0530

    reactive-form mod

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'reactive-form';
+  userForm!:FormGroup;
+
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      name : new FormControl(null,[Validators.required,Validators.minLength(3)]),
+      password : new FormControl(null,[Validators.required,Validators.minLength(6)]),
+      confirmPassword : new FormControl(null,Validators.required)
+    });
+  }
+
+  get name() {
+    return this.userForm.get('name');
+  }
+
+  get password() {
+    return this.userForm.get('password');
+  }
+  get confirmPassword() {
+    return this.userForm.get('confirmPassword');
+  }
+
+  onSubmit() {
+    let passwordValue = this.userForm.value.password;
+    let confirmPasswordValue = this.userForm.value.confirmPassword;
+    
+    if(passwordValue == confirmPasswordValue) {
+      alert("Form submitted successfully!");
+    } else {
+      alert("Passwords do not match");
+    }
+
+  }
 }
